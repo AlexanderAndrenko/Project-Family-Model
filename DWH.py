5e7d5a3efb3223c81eeed3e90319338b738815e3
Alexander Andrenko <andrenkom@gmail.com>
2023-03-26 22:26:37 +0700

    Continue implement insert methods

--- DWH.py
+++ DWH.py
@@ -8,18 +8,4 @@ import numpy as np
 
 Model.__init__()
 
-Session = sessionmaker(Model.engine)
-session = Session()
-
-total = sberETL.prepareDataFile()
-
-if not total.empty:
-    Model.TypeOperationInsert(total['Тип операции'].unique())
-    Model.CurrencyInsert(total['Валюта'].unique())
-    Model.DescriptionInsert(total['Описание'].unique())
-    Model.AccountInsert(total['Номер счета/карты зачисления'].unique())
-    Model.CategoryInsert(total['Категория'].unique())
-
-print(total['Номер счета/карты зачисления'].unique())
-
 session.close()
